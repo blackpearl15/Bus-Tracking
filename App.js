31a323761739789b7fb8d8ac55ca20a99d3442f0
Satyam7Jha <75986382+Satyam7Jha@users.noreply.github.com>
2021-08-29 00:42:10 +0530

    marker and gps icon

--- App.js
+++ App.js
@@ -1,29 +1,37 @@
 // import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View} from "react-native";
+import { StyleSheet, Text, View,Button} from "react-native";
 import Home from "./screens/Home";
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 
-export default function App() {
-  console.log("App executed");
-
+function HomeScreen({ navigation }) {
   return (
- 
-   <View style={styles.container}>
-     
-     <Home/>
-   </View>
-    
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Home/>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-  //  flex: 1,
-  //  backgroundColor: "#fff",
-  // justifyContent : 'center',
 
-  },
- 
-  
-});
+const Drawer = createDrawerNavigator();
+
+export default function App  () {
+  console.log("App executed");
+
+  return (
+
+         <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen name="Home" component={HomeScreen} />
+            <Drawer.Screen name="Add Bus" component={HomeScreen} />
+            <Drawer.Screen name="Profile" component={HomeScreen} />
+            <Drawer.Screen name="Setting" component={HomeScreen} />
+          </Drawer.Navigator>
+    </NavigationContainer>
+
+
+   
+  );
+}
